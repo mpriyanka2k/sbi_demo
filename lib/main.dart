aca784d57f62308f2961a08ffb44d41d588fba09
Priyanka Malekar <priyankamalekar2000@gmail.com>
2025-11-26 10:28:54 +0530

    1.Added encryption service class 2.Added flutter_screenutil package for dynamic UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sbi_demo/core/constant/app_strings.dart';
 import 'package:sbi_demo/core/network/network_info.dart' show NetworkInfo;
 import 'package:sbi_demo/core/router/app_router.dart';
@@ -14,12 +15,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
-    return MaterialApp.router(
-      title: AppStrings.appName,
-      theme: AppTheme.darkTheme,
-       routerConfig: AppRouter.router,
-    );
-   
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context,child) {
+        return MaterialApp.router(
+          title: AppStrings.appName,
+          theme: AppTheme.darkTheme,
+           routerConfig: AppRouter.router,
+        );
+      }
+    );  
   }
 }
